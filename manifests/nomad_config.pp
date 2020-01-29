6653450f9d5cd4459c9f698d906c9b45b4f25865
Anton Yakimov <anton.jakimov@ctco.lv>
2020-01-29 15:32:30 +0200

    - Bugfix

--- manifests/nomad_config.pp
+++ manifests/nomad_config.pp
@@ -6,9 +6,22 @@
 #   include nomad::nomad_config
 define nomad::nomad_config(
   String                            $setting = $title,
-  Variant[String, Integer]          $value   = '',
+  Variant[String, Integer, Struct]  $value   = '',
   Enum['present', 'absent']         $ensure  = 'present',
 ) {
+
+  if type($value) == String {
+    $line = "${setting} = \"${value}\""
+  }
+
+  if type($value) == Integer {
+    $line = "${setting} = ${value}"
+  }
+
+  if type($value) == Struct  {
+    $line = "${setting} = \"${value}\""
+  }
+
   file_line { "nomad-config-${setting}-${path}":
     ensure            => $ensure,
     path              => "${nomad::config_dir}/nomad.hcl",
