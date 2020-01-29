db083e9215cd9b723c3c0d18f6a151e4b0104fed
Anton Yakimov <anton.jakimov@ctco.lv>
2020-01-29 10:30:26 +0200

    - Initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,36 @@
-# @summary A short summary of the purpose of this class
+# == Class: nomad
 #
-# A description of what this class does
+# Installs, configures and manages nomad
 #
-# @example
-#   include nomad
-class nomad {
+class nomad (
+  String[1]                             $version            = '0.10.2',
+  String[1]                             $download_url_base  = 'https://releases.hashicorp.com/nomad/',
+  String[1]                             $download_extension = 'zip',
+  Optional[Stdlib::HTTPUrl]             $download_url       = undef,
+  Optional[Stdlib::HTTPUrl]             $proxy_server       = undef,
+  String[1]                             $arch               = $nomad::params::arch,
+  String[1]                             $package_ensure     = 'latest',
+  String[1]                             $package_name       = 'nomad',
+  String[1]                             $os                 = $facts['kernel'].downcase,
+  String[1]                             $user               = $nomad::params::user,
+  String[1]                             $group              = $nomad::params::group,
+  String[1]                             $init_style         = $nomad::params::init_style,
+  Hash                                  $config_defaults    = $nomad::params::config_defaults,
+  Hash                                  $config_hash        = {},
+) inherits nomad::params {
+
+  $real_download_url = pick(
+    $download_url,
+    "${download_url_base}${version}/${package_name}_${version}_${os}_${arch}.${download_extension}",
+  )
+
+  $user_real = $user
+  $group_real = $group
+  $init_style_real = $init_style
+
+  $config_hash_real = deep_merge($config_defaults, $config_hash)
+
+  contain 'nomad::install'
+
+  Class['nomad::install']
 }
---
+++ manifests/params.pp
@@ -0,0 +1,87 @@
+class nomad::params {
+
+  case $facts['architecture'] {
+    'x86_64', 'x64', 'amd64': { $arch = 'amd64' }
+    'i386':                   { $arch = '386'   }
+    'aarch64':                { $arch = 'arm64' }
+    /^arm.*/:                 { $arch = 'arm'   }
+    default:                  {
+      fail("Unsupported kernel architecture: ${facts['architecture']}")
+    }
+  }
+
+  $config_dir = $facts['os']['family'] ? {
+    'windows' => 'C:\\ProgramData\\nomad\\config',
+    default   => '/etc/nomad'
+  }
+
+  $bin_dir = $facts['os']['family'] ? {
+    'windows' => 'C:\\ProgramData\\nomad',
+    default   => '/usr/local/bin'
+  }
+
+  case $facts['os']['name'] {
+    'windows': {
+      $data_dir_mode = '0775'
+      $binary_group = undef
+      $binary_mode = '0775'
+      $binary_name = 'nomad.exe'
+      $binary_owner = 'NT AUTHORITY\NETWORK SERVICE'
+      $config_defaults  = {
+        data_dir => 'C:\\ProgramData\\nomad'
+      }
+      $manage_user = false
+      $manage_group = false
+      $user = 'NT AUTHORITY\NETWORK SERVICE'
+      $group = 'Administrators'
+    }
+    default: {
+      # 0 instead of root because OS X uses "wheel".
+      $data_dir_mode = '0755'
+      $binary_group = '0'
+      $binary_mode = '0555'
+      $binary_name = 'nomad'
+      $binary_owner = 'root'
+      $config_defaults  = {
+        data_dir => '/opt/nomad'
+      }
+      $manage_user = true
+      $manage_group = true
+      $user = 'nomad'
+      $group = 'nomad'
+    }
+  }
+
+  case $facts['os']['name'] {
+    'Ubuntu': {
+      $shell = '/usr/sbin/nologin'
+    }
+    'RedHat': {
+      $shell = '/sbin/nologin'
+    }
+    'Debian': {
+      $shell = '/usr/sbin/nologin'
+    }
+    'Archlinux': {
+      $shell = '/sbin/nologin'
+    }
+    'OpenSuSE': {
+      $shell = '/usr/sbin/nologin'
+    }
+    /SLE[SD]/: {
+      $shell = '/usr/sbin/nologin'
+    }
+    default: {
+      $shell = undef
+    }
+  }
+
+  if $facts['os']['family'] == 'windows' {
+    $init_style = 'unmanaged'
+  } else {
+    $init_style = $facts['service_provider'] ? {
+      undef   => 'systemd',
+      default => $facts['service_provider']
+    }
+  }
+}
